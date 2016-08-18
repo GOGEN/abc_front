ecd006f74d27e4369079fb8cdff07b3ba3126a04
Marat Latypov <gogen.marat@gmail.com>
2016-08-18 19:27:32 +0300

    fix bug with form jumping on mobile nrowser

--- src/scripts/popups.js
+++ src/scripts/popups.js
@@ -15,11 +15,32 @@
   });
 
 
-  $('a[href^=\\#popup_call]').magnificPopup({
-    type:'inline',
-    mainClass: 'popup-call-override',
-    fixedContentPos: true
-  });
+  var startWindowScroll = 0;
+
+  if (!Modernizr.touchevents) {
+    $('a[href^=\\#popup_call]').magnificPopup({
+      type:'inline',
+      mainClass: 'popup-call-override',
+      fixedContentPos: true
+    });
+  } else {
+    $('a[href^=\\#popup_call]').magnificPopup({
+      type:'inline',
+      mainClass: 'popup-call-override',
+      fixedContentPos: false,
+      callbacks: {
+        beforeOpen: function () {
+          startWindowScroll = $(window).scrollTop();
+          $('html').addClass('mfp-helper');
+
+        },
+        close: function () {
+          $('html').removeClass('mfp-helper');
+          $(window).scrollTop(startWindowScroll);
+        }
+      }
+    });
+  }
 
   $('a[href^=\\#popup_photo').magnificPopup({
     type: 'inline',
