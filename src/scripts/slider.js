710bf8265d725d58db5c2d6db3d0c1550cf71763
Marat Latypov <gogen.marat@gmail.com>
2016-08-19 12:08:40 +0300

    fix slider on product page 9

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -4,25 +4,28 @@
   $(document).ready(function () {
     var sliders = $('[id^="product_slick_slider_item"]');
     if ($(window).width() > 980) {
-      sliders.find('#product_slick_slider_content').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: sliders.find('#product_slick_slider_prev'),
-        nextArrow: sliders.find('#product_slick_slider_next'),
-        variableWidth: true,
-        centerMode: true
+      sliders.toArray().forEach(function (slider){
+        $(slider).find('#product_slick_slider_content').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: $(slider).find('#product_slick_slider_prev'),
+          nextArrow: $(slider).find('#product_slick_slider_next'),
+          variableWidth: true,
+          centerMode: true
+        });
       });
     } else {
-      sliders.find('#product_slick_slider_content').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-        arrows: false,
-        variableWidth: true
+      sliders.forEach(function (slider) {
+        $(slider).find('#product_slick_slider_content').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false,
+          variableWidth: true
+        });
       });
     }
   });
-
 })();
