985c284d82d2d79d1410abadea0c5edb653db002
Marat Latypov <gogen.marat@gmail.com>
2016-08-19 11:38:25 +0300

    Update 2016-08-19T08:38:22.602Z

--- main.js
+++ main.js
@@ -326,27 +326,28 @@
 
 (function () {
   'use strict';
-
-  var sliders = $('[id^="product_slick_slider_item"]');
-  if ($(window).width() > 980) {
-    sliders.find('#product_slick_slider_content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow: sliders.find('#product_slick_slider_prev'),
-      nextArrow: sliders.find('#product_slick_slider_next'),
-      variableWidth: true,
-      centerMode: true
-    });
-  } else {
-    sliders.find('#product_slick_slider_content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-      arrows: false,
-      variableWidth: true
-    });
-  }
+  $(document).ready(function () {
+    var sliders = $('[id^="product_slick_slider_item"]');
+    if ($(window).width() > 980) {
+      sliders.find('#product_slick_slider_content').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: sliders.find('#product_slick_slider_prev'),
+        nextArrow: sliders.find('#product_slick_slider_next'),
+        variableWidth: true,
+        centerMode: true
+      });
+    } else {
+      sliders.find('#product_slick_slider_content').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        arrows: false,
+        variableWidth: true
+      });
+    }
+  });
 
 })();
