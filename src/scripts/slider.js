e388e05522f50c3c696a4189141520ce5a596445
Marat Latypov <gogen.marat@gmail.com>
2016-08-19 08:08:46 +0300

    fix slider on product page 5

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -3,26 +3,15 @@
   'use strict';
 
   var sliders = $('[id^="product_slick_slider_item"]');
-  if ($(window).width() > 980) {
-    sliders.find('#product_slick_slider_content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow: sliders.find('#product_slick_slider_prev'),
-      nextArrow: sliders.find('#product_slick_slider_next'),
-      variableWidth: true,
-      centerMode: true
-    });
-  } else {
-    sliders.find('#product_slick_slider_content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-      arrows: false,
-      mobileFirst: true,
-      variableWidth: true
-    });
-  }
+  sliders.find('#product_slick_slider_content').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: sliders.find('#product_slick_slider_prev'),
+    nextArrow: sliders.find('#product_slick_slider_next'),
+    variableWidth: true,
+    centerMode: true,
+    mobileFirst: true
+  });
 
 })();
