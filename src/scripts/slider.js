e948c5fb2151bf8127632787e9011ad2232744e2
Marat Latypov <gogen.marat@gmail.com>
2016-08-19 13:15:33 +0300

    fix slider on product page 13

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,35 +1,31 @@
 
 (function () {
   'use strict';
-  setTimeout(function () {
+  $(document).ready(function () {
     var sliders = $('[id^="product_slick_slider_item"]');
     if ($(window).width() > 980) {
       sliders.toArray().forEach(function (slider){
-        setTimeout(function () {
-          $(slider).find('#product_slick_slider_content').slick({
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            prevArrow: $(slider).find('#product_slick_slider_prev'),
-            nextArrow: $(slider).find('#product_slick_slider_next'),
-            variableWidth: true,
-            centerMode: true
-          });
-        }, 5000);
+        $(slider).find('#product_slick_slider_content').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: $(slider).find('#product_slick_slider_prev'),
+          nextArrow: $(slider).find('#product_slick_slider_next'),
+          variableWidth: true,
+          centerMode: true
+        });
       });
     } else {
       sliders.toArray().forEach(function (slider) {
-        setTimeout(function () {
-          $(slider).find('#product_slick_slider_content').slick({
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerMode: true,
-            arrows: false,
-            variableWidth: true
-          });
-        }, 5000)
+        $(slider).find('#product_slick_slider_content').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false,
+          variableWidth: true
+        });
       });
     }
-  }, 5000);
+  });
 })();
