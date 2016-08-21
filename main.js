9466b4ceff7de563688752d242cda6702ec35034
Marat Latypov <gogen.marat@gmail.com>
2016-08-21 17:32:38 +0300

    Update 2016-08-21T14:32:34.853Z

--- main.js
+++ main.js
@@ -217,29 +217,45 @@
 (function () {
   'use strict';
 
-  if ($(window).width() > 980) {
-    return;
-  }
-  var slideout = new Slideout({
-        'panel': $('.slide-wrapper')[0],
-        'menu': $('#menu')[0],
-        'padding': 980,
-        'tolerance': 70
-      });
-
+  var slideout;
   var toggleButton = $('.navigation-checkbox-label');
 
-  toggleButton.click(function () {
-    slideout.toggle();
-  });
+  function initSlideout() {
+    if (slideout) {
+      toggleButton.off('click', slideout.toggle.bind(slideout));
+      slideout.destroy();
+    }
 
-  slideout.on('open', function() {
-    toggleButton.addClass('navigation-checkbox-label--open');
-  });
+    if ($(window).width() > 980) {
+      if (slideout) {
+        $(slideout.panel).removeClass('slideout-panel');
+        $(slideout.menu).removeClass('slideout-menu');
+      }
+      return;
+    }
 
-  slideout.on('close', function() {
-    toggleButton.removeClass('navigation-checkbox-label--open');
-  });
+    slideout = new Slideout({
+      panel: $('.slide-wrapper')[0],
+      menu: $('#menu')[0],
+      padding: $(window).width(),
+      tolerance: 70,
+      fx: 'linear',
+      duration: 300
+    });
+
+    toggleButton.on('click', slideout.toggle.bind(slideout));
+
+    slideout.on('beforeopen', function() {
+      toggleButton.addClass('navigation-checkbox-label--open');
+    });
+
+    slideout.on('beforeclose', function() {
+      toggleButton.removeClass('navigation-checkbox-label--open');
+    });
+
+  }
+
+  initSlideout();
 
   var selectors = ['[id^="product_slick_slider_item"]', '#carousel', '#news_gallery_view', '#news_gallery_list'];
 
@@ -247,10 +263,14 @@
     $(selector).on('touchstart', function (event) {
       slideout.disableTouch();
     });
-
     $(selector).on('touchend', function (event) {
       slideout.enableTouch();
     });
+  });
+
+
+  $(window).resize(function () {
+    initSlideout();
   });
 
 })();
